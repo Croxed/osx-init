45263b10f312c0efd8a854403ae2ee3304b6d1bb
Oscar Wennergren <owlivee@gmail.com>
2016-01-26 16:51:39 +0100

    Added iTerm2 installation and Solarized Dark-theme for iTerm2.

--- install.sh
+++ install.sh
@@ -10,4 +10,12 @@ cp ZSH/aliases.zsh ~/.oh-my-zsh/custom/
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sh dotfiles/.osx
 brew install screenfetch
+brew cask install iterm2
+mkdir -p tempInstall
+cd tempInstall
+curl -LOk https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
+mv Solarized%20Dark.itermcolors Solarized Dark.itermcolors
+open Solarized Dark.itermcolors
+cd ..
+rm -rf tempInstall
 zr
