85d35af5adbed08889fc87fa11e3fd984605893b
Oscar Wennergren <owlivee@gmail.com>
2016-01-26 16:38:50 +0100

    Woop

--- QLPluginsInstaller.sh
+++ QLPluginsInstaller.sh
@@ -13,11 +13,11 @@ brew tap caskroom/cask
 brew cask install betterzipql invisorql provisionql qlcolorcode qlimagesize qlmarkdown qlprettypatch quicklook-csv quicklook-json quicknfo scriptql suspicious-package webpquicklook
 echo Installing/Downloading Quick Look-plugins that are not from Homebrew 
 echo THEY MIGHT BE OUTDATED
-cp QLPlugins/*.qlgenerator ~/Library/QuickLook
-curl -o ~/Library/QuickLook/ http://repo.whine.fr/qlmoviepreview.qlgenerator-10.9.zip
-curl -o ~/Library/QuickLook/ http://ipaql.com/site/assets/files/1006/ipaql_1-3-0.zip
-curl -o ~/Library/QuickLook/ https://www.kainjow.com/downloads/ScriptQL_qlgenerator.zip
-curl -o ~/Library/QuickLook/ http://blog.timac.org/post-images/StringsFileQuickLook/StringsFile.qlgenerator.zip
+cp QLPlugins/*.qlgenerator ~/Library/QuickLook/
+curl -o ~/Library/QuickLook/1.zip http://repo.whine.fr/qlmoviepreview.qlgenerator-10.9.zip
+curl -o ~/Library/QuickLook/2.zip http://ipaql.com/site/assets/files/1006/ipaql_1-3-0.zip
+curl -o ~/Library/QuickLook/3.zip https://www.kainjow.com/downloads/ScriptQL_qlgenerator.zip
+curl -o ~/Library/QuickLook/4.zip http://blog.timac.org/post-images/StringsFileQuickLook/StringsFile.qlgenerator.zip
 cd ~/Library/QuickLook
 for filename in ./*.zip; do
 	unzip $filename
