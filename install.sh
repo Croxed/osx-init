c0743f143ecf5d55e3d7f660221b1ebc86129ff4
Oscar Wennergren <owlivee@gmail.com>
2016-01-26 15:12:44 +0100

    Command Line Tools added and also cloned syntax highlighting for ZSH

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo "This scripts will configure OS X with Homebrew and will do some fine tuning to the OS"
+echo "Installing Xcode Command Line Tools"
+xcode-select install
 sh QLPluginsInstaller.sh
 sh dotfiles/.osx
 sh dotfiles/brew.sh
@@ -7,4 +9,3 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 cp ZSH/.zshrc ~/.zshrc
 cp ZSH/aliases.zsh ~/.oh-my-zsh/custom
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sudo xcodebuild -license
