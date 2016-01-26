86746cb51d7285439c35e5f3f4c572a3f18260d5
Oscar Wennergren <owlivee@gmail.com>
2016-01-26 16:04:10 +0100

    Once more..

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ echo "This scripts will configure OS X with Homebrew and will do some fine tunin
 echo "Installing Xcode Command Line Tools"
 xcode-select --install
 sh QLPluginsInstaller.sh
-sh dotfiles/.osx
 sh dotfiles/brew.sh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ZSH/.zshrc ~/.zshrc
 cp ZSH/aliases.zsh ~/.oh-my-zsh/custom
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sh dotfiles/.osx
