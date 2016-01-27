3b4bd99642accd227cc6454bbba0c78f87fb56dd
Oscar Wennergren <owlivee@gmail.com>
2016-01-27 14:04:37 +0100

    Forgot to remove this..

--- QLPluginsInstaller.sh
+++ QLPluginsInstaller.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
-echo "Updating Brew or installing if Homebrew doesn't exist" 
-which -s brew
-if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-	brew update && brew outdated && brew upgrade && brew cleanup
-fi
 mkdir -p ~/Library/QuickLook
 echo Installing Quick Look-plugins from Homebrew
 brew tap caskroom/cask
