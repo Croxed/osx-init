aecbde2afbc043f703bc7e2d8468aef73410ab2b
Oscar Wennergren <owlivee@gmail.com>
2016-01-27 21:02:25 +0100

    Force brewInstall to wait untill xcode devel tools are installed

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "This scripts will configure OS X with Homebrew and will do some fine tuning to the OS"
 echo "Installing Xcode Command Line Tools"
-xcode-select --install
+xcode-select --install & wait
 sh brewInstall.sh
 sh QLPluginsInstaller.sh
 sh dotfiles.sh
