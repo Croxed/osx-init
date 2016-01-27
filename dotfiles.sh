bd344c990411ea66132d6964cb367aeb366e9a6a
Oscar Wennergren <owlivee@gmail.com>
2016-01-27 14:17:26 +0100

    Minor changes, may correct some errors

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-sh dotfiles/brew.sh
+sh dotfiles/brew.sh 
 mkdir -p tempInstall
 cd tempInstall
+brew instlal wget
 wget -c https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
 wget -c https://iterm2.com/downloads/stable/iTerm2-2_1_4.zip
 unzip iTerm2-2_1_4.zip
--- install.sh
+++ install.sh
@@ -4,16 +4,11 @@ echo "Installing Xcode Command Line Tools"
 xcode-select --install
 sh brewInstall.sh
 sh QLPluginsInstaller.sh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-mkdir -p tempInstall
-cd tempInstall
-brew install wget
-wget -C https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
-open Solarized Dark.itermcolors
-cd ..
-rm -rf tempInstall
-cp ZSH/.zshrc ~/.zshrc
-cp ZSH/aliases.zsh ~/.oh-my-zsh/custom/
-source ~/.zshrc
-sh dotfiles.sh
+#echo "Installing oh-my-zsh"
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#echo "Installing zsh-syntax-highlighting"
+#git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+#cp ZSH/.zshrc ~/.zshrc
+#cp ZSH/aliases.zsh ~/.oh-my-zsh/custom/
+#source ~/.zshrc
+sh dotfiles.sh && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && cp ZSH/.zshrc ~/.zshrc && cp ZSH/aliases.zsh ~/.oh-my-zsh/custom/ && source ~/.zshrc
