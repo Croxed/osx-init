6cdb1a9e3da88373ec1adeff2f8fa24232a433b8
Oscar Wennergren <owlivee@gmail.com>
2016-01-27 18:31:41 +0100

    Tuned the if-statement

--- brewInstall.sh
+++ brewInstall.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 echo "Updating Brew or installing if Homebrew doesn't exist" 
 which -s brew
-if [[ $? != 0 ]] ; then
+if ! type "brew" > /dev/null ; then
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update && brew outdated && brew upgrade && brew cleanup
 else
+	echo "Brew already installed"
 	brew update && brew outdated && brew upgrade && brew cleanup
 fi
