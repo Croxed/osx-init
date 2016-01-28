da678f68bfec73633b7c4d69de2930c4176c2145
Oscar Wennergren <owlivee@gmail.com>
2016-01-28 14:04:44 +0100

    Trying to make oh-my-zsh quit after install

--- dotfiles.sh
+++ dotfiles.sh
@@ -13,10 +13,22 @@ then
     wget -O /tmp/oh-my-zsh.command https://raw.github.com/Croxed/OSX-Fresh-Install/master/oh-my-zsh.command
 fi
 chmod +x /tmp/oh-my-zsh.command
-open /tmp/oh-my-zsh.command
+(open /tmp/oh-my-zsh.command && closeWindow(); \
+curl -o ~/.zshrc https://raw.github.com/Croxed/OSX-Fresh-Install/master/ZSH/.zshrc ; \
+curl -o  ~/.oh-my-zsh/custom/aliases.zsh https://raw.github.com/Croxed/OSX-Fresh-Install/master/ZSH/aliases.zsh ; \
+source ~/.zshrc)
 cd ..
 rm -rf tempInstall
 echo "Installing zsh-syntax-highlighting"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-(curl -o ~/.zshrc https://raw.github.com/Croxed/OSX-Fresh-Install/master/ZSH/.zshrc ; curl -o  ~/.oh-my-zsh/custom/aliases.zsh https://raw.github.com/Croxed/OSX-Fresh-Install/master/ZSH/aliases.zsh ; source ~/.zshrc)
 sh -c "$(curl -fsSL https://raw.github.com/Croxed/OSX-Fresh-Install/master/dotfiles/.osx)"
+
+closeWindow() {
+    /usr/bin/osascript << _OSACLOSE_
+    tell application "Terminal"
+        close (every window whose name contains "oh-my-zsh")
+    end tell
+    delay 0.3
+    tell application "System Events" to click UI element "Close" of sheet 1 of window 1 of application process "Terminal"
+_OSACLOSE_
+}
