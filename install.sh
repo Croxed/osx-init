061774adc306b3c1ef3bd0327c5b8d66c2108047
Oscar Wennergren <owlivee@gmail.com>
2016-01-28 09:45:16 +0100

    Installing xcode dev-tools via cli and matching to OS X-version

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 #! /bin/bash
 echo "This scripts will configure OS X with Homebrew and will do some fine tuning to the OS"
 echo "Installing Xcode Command Line Tools"
-check=$(((xcode-select --install) 2>&1))
-echo $check
-str="xcode-select: note: install requested for command line developer tools"
-while [[ "$check" == "$str" ]];
-do
-  osascript -e 'tell app "System Events" to display dialog "xcode command-line tools missing." buttons "OK" default button 1 with title "xcode command-line tools"'
-  exit;  
-done
-
+sh xcode-cli-install.sh
 sh brewInstall.sh
 sh QLPluginsInstaller.sh
 sh dotfiles.sh
