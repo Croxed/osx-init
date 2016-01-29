236b216e40b27566a8cf57527a05d4a24217a15b
Oscar Wennergren <owlivee@gmail.com>
2016-01-29 14:35:10 +0100

    Added way to keep root during script

--- scripts/QLPluginsInstaller.sh
+++ scripts/QLPluginsInstaller.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 mkdir -p ~/Library/QuickLook
 echo Installing Quick Look-plugins from Homebrew
 brew tap caskroom/cask
--- scripts/brewInstall.sh
+++ scripts/brewInstall.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "Installing Xcode Command Line Tools"
 sh -c "$(curl -fsSL https://raw.github.com/Croxed/osx-init/master/scripts/xcode-cli-install.sh)"
 echo "Updating Brew or installing if Homebrew doesn't exist" 
