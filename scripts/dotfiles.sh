f6100ad2053d2f683e6294cd45278c6aa470d9ec
Oscar Wennergren <owlivee@gmail.com>
2016-02-01 10:19:05 +0100

    Added option to backup your files to the desktop if they exist

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,4 +1,34 @@
 #! /bin/zsh
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:- Are you sure? y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+makeBackup() {
+	dir=~/Desktop/OSX-INIT-BACKUP
+	# Make backup-folder for .zshrc and aliases.zsh
+	if [ ! -d "$dir" ] ; then
+		mkdir -p $dir
+	fi
+
+	if [ ! -f "$dir/restore.sh" ]; then
+		wget -c https://raw.github.com/Croxed/osx-init/master/scripts/restore.sh
+	fi
+
+	if [[ -n $1 ]]; then
+		mv $1 $dir/
+	fi
+}
+
 # Ask for the administrator password upfront.
 sudo -v
 
@@ -22,14 +52,28 @@ defaults read com.googlecode.iterm2 &&
 open /Applications/iTerm.app
 
 # Downloading oh-my-zsh
-if [ ! -f "/tmp/oh-my-zsh.sh" ]
-then
+if [ ! -f "/tmp/oh-my-zsh.sh" ] ; then
     (curl -LOk https://raw.github.com/Croxed/osx-init/master/scripts/oh-my-zsh.sh; mv oh-my-zsh.sh /tmp/oh-my-zsh.sh; sh /tmp/oh-my-zsh.sh & wait)
 else
 	sh /tmp/oh-my-zsh.sh & wait
 fi
+
+if [ ! -f ~/.zshrc ] ; then
+	wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/.zshrc -O ~/.zshrc
+else
+	confirm "It seems that you already have a .zshrc-file. Do you want to replace it? y/N" && 
+	makeBackup ~/.zshrc &&
+	wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/.zshrc -O ~/.zshrc	
+fi
+
+if [ ! -f ~/.oh-my-zsh/custom/aliases.zsh ] ; then
+	wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/aliases.zsh -O ~/.oh-my-zsh/custom/aliases.zsh
+else
+	confirm "It seems that you already have a aliases.zsh-file. Do you want to replace it? y/N" && 
+	makeBackup ~/.oh-my-zsh/custom/aliases.zsh &&
+	wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/aliases.zsh -O ~/.oh-my-zsh/custom/aliases.zsh	
+fi
+
 echo "Installing zsh-syntax-highlighting"
-(wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/.zshrc -O ~/.zshrc ; wget -c https://raw.github.com/Croxed/osx-init/master/ZSH/aliases.zsh -O ~/.oh-my-zsh/custom/aliases.zsh)
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-source ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/Croxed/osx-init/master/dotfiles/.osx)"
