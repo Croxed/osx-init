eb7d0f39bfa8022577e3539dee8d11b3149455c7
Oscar Wennergren <owlivee@gmail.com>
2016-02-03 01:26:36 +0100

    Corrected minor errors

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if [ -f ".zshrc"] ; then
+if [ -f ".zshrc" ] ; then
 	mv .zshrc ~/.zshrc
 fi
 
-if [ -f "aliases.zsh"] ; then
+if [ -f "aliases.zsh" ] ; then
 	mv aliases.zsh ~/oh-my-zsh/custom/aliases.zsh
 fi
