83ba0e3b6e26e1fc9900554447fc91ec4b2b594b
Oscar Wennergren <owlivee@gmail.com>
2016-02-03 13:31:59 +0100

    Added confirmation to restore

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,8 +1,27 @@
 #! /bin/bash
-if [ -f ".zshrc" ] ; then
-	mv .zshrc ~/.zshrc
-fi
 
-if [ -f "aliases.zsh" ] ; then
-	mv aliases.zsh ~/oh-my-zsh/custom/aliases.zsh
-fi
+restore () {
+	# Restoring .zshrc and aliases.zsh
+	if [ -f ".zshrc" ] ; then
+		mv .zshrc "$HOME/.zshrc"
+	fi
+
+	if [ -f "aliases.zsh" ] ; then
+		mv aliases.zsh "$HOME/oh-my-zsh/custom/aliases.zsh"
+	fi
+}
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:- Are you sure? y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm "Do you want to restore to your old .zshrc and aliases.zsh? y/N" && restore
